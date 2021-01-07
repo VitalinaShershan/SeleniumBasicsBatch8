c3d21c5701dc7bea3b5e72f92290786e66ddee3e
SyntaxTechnologies <instructor@syntaxtechs.com>
2021-01-06 21:48:59 -0500

    POM intro

---
+++ com/syntax/testbase/BaseClass.java
@@ -0,0 +1,58 @@
+package com.syntax.testbase;
+
+import com.syntax.utils.ConfigsReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+
+    public static WebDriver driver;
+
+    /**
+     * this method will open a browser, set up configuration and navigate to the URL
+     */
+    public static void setUp() {
+
+        ConfigsReader.readProperties("configs/configuration.properties");
+        switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Ivalid browser");
+        }
+        driver.get(ConfigsReader.getPropertyValue("url"));
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    /**
+     * this method will navigate a website by the given URL
+     * @param url
+     */
+    public static void setUpWithSpecificURL(String url) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    /**
+     * this method will close any open browser
+     */
+    public static void tearDown(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+
+}
--- com/syntax/utils/CommonMethods.java
+++ com/syntax/utils/CommonMethods.java
@@ -1,5 +1,6 @@
 package com.syntax.utils;
 
+import com.syntax.testbase.BaseClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,40 +8,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class CommonMethods {
-
-    protected static WebDriver driver;
-
-    /**
-     * this method will open a browser, set up configuration and navigate to the URL
-     */
-    public static void setUp() {
-
-        ConfigsReader.readProperties("configs/configuration.properties");
-        switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new RuntimeException("Ivalid browser");
-        }
-        driver.get(ConfigsReader.getPropertyValue("url"));
-        //driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    /**
-     * this method will close any open browser
-     */
-    public static void tearDown(){
-        if(driver!=null){
-            driver.quit();
-        }
-    }
+public class CommonMethods extends BaseClass {
 
 }
